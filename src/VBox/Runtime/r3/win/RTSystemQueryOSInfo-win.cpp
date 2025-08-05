bcd3d8dea2d7915822b73d6e42a10db98eaa36bf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-08-05 13:45:50 +0000

    IPRT/win: Adding the update build revision number to RTSYSOSINFO_RELEASE. Introduced a RTWinSysRegXxx API for getting register values.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110137 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Runtime/r3/win/RTSystemQueryOSInfo-win.cpp
+++ src/VBox/Runtime/r3/win/RTSystemQueryOSInfo-win.cpp
@@ -46,9 +46,10 @@
 #include <iprt/system.h>
 #include <iprt/assert.h>
 #include <iprt/ctype.h>
-#include <iprt/errcore.h>
+#include <iprt/err.h>
 #include <iprt/string.h>
 #include <iprt/utf16.h>
+#include <iprt/win/reg.h>
 
 
 /*********************************************************************************************************************************
@@ -264,8 +265,19 @@ static int rtSystemWinQueryOSVersion(RTSYSOSINFO enmInfo, char *pszInfo, size_t 
          */
         case RTSYSOSINFO_RELEASE:
         {
-            RTStrPrintf(szTmp, sizeof(szTmp), "%u.%u.%u",
-                        g_WinOsInfoEx.dwMajorVersion, g_WinOsInfoEx.dwMinorVersion, g_WinOsInfoEx.dwBuildNumber);
+            uint32_t uUpdateBuildRev = 0;
+            int vrc = RTWinRegQueryValueU32(kRTWinRegRoot_LocalMachine,
+                                            L"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion", L"UBR", &uUpdateBuildRev);
+            if (RT_SUCCESS(vrc))
+                RTStrPrintf(szTmp, sizeof(szTmp), "%u.%u.%u.%u",
+                            g_WinOsInfoEx.dwMajorVersion, g_WinOsInfoEx.dwMinorVersion, g_WinOsInfoEx.dwBuildNumber,
+                            uUpdateBuildRev);
+            else
+            {
+                Assert(vrc == VERR_FILE_NOT_FOUND || vrc == VERR_PATH_NOT_FOUND || vrc == VERR_NOT_FOUND);
+                RTStrPrintf(szTmp, sizeof(szTmp), "%u.%u.%u",
+                            g_WinOsInfoEx.dwMajorVersion, g_WinOsInfoEx.dwMinorVersion, g_WinOsInfoEx.dwBuildNumber);
+            }
             break;
         }
 
