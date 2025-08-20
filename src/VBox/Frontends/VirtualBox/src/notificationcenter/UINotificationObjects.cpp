94905f46877d8917bc9a3deaa5c08bb5d09afdaf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-08-20 12:56:12 +0000

    FE/Qt: The VirtualBox GUI was not displaying errors when calls to IMachine::takeSnapshot(), IMachine::deleteSnapshot(), and ICloudClient::cloneInstance() failed due to checking the wrong COM wrapper (for example: m_comMachine vs. comMachine) for the result. github:gh-75
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110315 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.cpp
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.cpp
@@ -3287,7 +3287,7 @@ CProgress UINotificationProgressCloudMachineClone::createProgress(COMResult &com
     CCloudMachine comCloneMachine;
     CProgress comProgress = m_comClient.CloneInstance(m_strId, m_strCloneName, comCloneMachine);
     /* Store COM result: */
-    comResult = m_comMachine;
+    comResult = m_comClient;
     /* Return progress-wrapper: */
     return comProgress;
 }
@@ -3794,7 +3794,7 @@ CProgress UINotificationProgressSnapshotTake::createProgress(COMResult &comResul
                                                     m_strSnapshotDescription,
                                                     true, m_uSnapshotId);
     /* Store COM result: */
-    comResult = m_comMachine;
+    comResult = comMachine;
     /* Return progress-wrapper: */
     return comProgress;
 }
@@ -4019,7 +4019,7 @@ CProgress UINotificationProgressSnapshotDelete::createProgress(COMResult &comRes
     /* Initialize progress-wrapper: */
     CProgress comProgress = comMachine.DeleteSnapshot(m_uSnapshotId);
     /* Store COM result: */
-    comResult = m_comMachine;
+    comResult = comMachine;
     /* Return progress-wrapper: */
     return comProgress;
 }
