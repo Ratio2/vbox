b515c4abae2f1dce4fa427275704461025b122ea
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-09-01 16:45:12 +0000

    FE/Qt: Buiuld fix for r170376.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110382 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.cpp
+++ src/VBox/Frontends/VirtualBox/src/notificationcenter/UINotificationObjects.cpp
@@ -738,10 +738,10 @@ void UINotificationMessage::cannotAcquireSessionParameter(const CSession &comSes
 /* static */
 void UINotificationMessage::cannotAcquireMachineParameter(const CMachine &comMachine)
 {
-    /* Do not show error for the NS_ERROR_NOT_IMPLEMENTED case, just add it to the log: */
-    if (comMachine.lastRC() == NS_ERROR_NOT_IMPLEMENTED)
+    /* Do not show error for the E_NOTIMPL case, just add it to the log: */
+    if (comMachine.lastRC() == E_NOTIMPL)
     {
-        LogRel(("GUI: IMachine getter lastRC == NS_ERROR_NOT_IMPLEMENTED, skipping ...\n"));
+        LogRel(("GUI: IMachine getter lastRC == E_NOTIMPL, skipping ...\n"));
         return;
     }
 
