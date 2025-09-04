ec2fd3a8e388e684d398aa91aee214f65c4648b6
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-09-04 10:24:47 +0000

    Devices/Video,Main: Restore enumerating available webcams for macOS, bugref:10953 [scm]
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110421 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Main/src-server/darwin/HostGetWebcamDarwin.mm
+++ src/VBox/Main/src-server/darwin/HostGetWebcamDarwin.mm
@@ -49,7 +49,7 @@ typedef FNVBOXHOSTWEBCAMADD *PFNVBOXHOSTWEBCAMADD;
 
 DECLHIDDEN(int) DarwinHostWebcamList(PFNVBOXHOSTWEBCAMADD pfnWebcamAdd, void *pvUser, uint64_t *pu64WebcamAddResult)
 {
-    int rc = VINF_SUCCESS;
+    int vrc = VINF_SUCCESS;
 
     NSAutoreleasePool* localpool = [[NSAutoreleasePool alloc] init];
 
@@ -81,21 +81,21 @@ DECLHIDDEN(int) DarwinHostWebcamList(PFNVBOXHOSTWEBCAMADD pfnWebcamAdd, void *pv
 
         if (pszAlias)
         {
-            rc = pfnWebcamAdd(pvUser,
-                              device.localizedName.UTF8String,
-                              device.uniqueID.UTF8String,
-                              pszAlias,
-                              pu64WebcamAddResult);
+            vrc = pfnWebcamAdd(pvUser,
+                               device.localizedName.UTF8String,
+                               device.uniqueID.UTF8String,
+                               pszAlias,
+                               pu64WebcamAddResult);
         }
         else
-            rc = VERR_NO_MEMORY;
+            vrc = VERR_NO_MEMORY;
 
         RTStrFree(pszAlias);
 
-        if (RT_FAILURE(rc))
+        if (RT_FAILURE(vrc))
             break;
     }
 
     [localpool drain];
-    return rc;
+    return vrc;
 }
