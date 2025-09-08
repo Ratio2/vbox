786d6ad7ae0d84606cff6c61ebe288ddd21b5cfe
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-09-08 17:31:20 +0000

    e1000: bugref:10239 Yet another build fix for r170458
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110460 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Devices/Network/DevE1000.cpp
+++ src/VBox/Devices/Network/DevE1000.cpp
@@ -800,7 +800,7 @@ struct E1kRxDStatusLegacy_st
 typedef struct E1kRxDStatusLegacy_st E1KRXDST;
 AssertCompileSize(E1KRXDST, 4);
 
-/** @todo: spec ref! */
+/** @todo spec ref! */
 struct E1kRxDStatusExtended_st
 {
     /** @name Extended Status field (7.1.4.6)
