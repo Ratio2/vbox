3c7db019767dc46235087010820150fa4774b7bb
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-09-25 21:12:26 +0000

    VMM/IOM: Added a IOMMMIO_FLAGS_SET_HI_OFF_BIT_READING_MISSING to the MMIO registration flags, making the complicated write code optionally set IOMMMIO_OFF_F_READING_MISSING_BYTES in the 'off' parameter to reads of missing bytes in writes.  This is to help make all the potentially missing bytes available thru the existing FNIOMMMIONEWREAD interface.  For use in DevTpm. bugref:10777 jiraref:VBP-1884
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110642 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/IOMAllMmioNew.cpp
+++ src/VBox/VMM/VMMAll/IOMAllMmioNew.cpp
@@ -207,9 +207,10 @@ static VBOXSTRICTRC iomMmioDoComplicatedWrite(PVM pVM, PVMCPU pVCpu, CTX_SUFF(PI
         uint32_t u32MissingValue = 0;
         if (fReadMissing && cbThisPart != 4)
         {
-            VBOXSTRICTRC rc2 = pRegEntry->pfnReadCallback(pRegEntry->pDevIns, pRegEntry->pvUser,
-                                                          !(pRegEntry->fFlags & IOMMMIO_FLAGS_ABS)
-                                                          ? offRegion & ~(RTGCPHYS)3 : (GCPhys & ~(RTGCPHYS)3),
+            RTGCPHYS offArgRead = (!(pRegEntry->fFlags & IOMMMIO_FLAGS_ABS) ? offRegion & ~(RTGCPHYS)3 : (GCPhys & ~(RTGCPHYS)3))
+                                | (!(pRegEntry->fFlags & IOMMMIO_FLAGS_SET_HI_OFF_BIT_READING_MISSING)
+                                   ? 0 : IOMMMIO_OFF_F_READING_MISSING_BYTES);
+            VBOXSTRICTRC rc2 = pRegEntry->pfnReadCallback(pRegEntry->pDevIns, pRegEntry->pvUser, offArgRead,
                                                           &u32MissingValue, sizeof(u32MissingValue));
             switch (VBOXSTRICTRC_VAL(rc2))
             {
