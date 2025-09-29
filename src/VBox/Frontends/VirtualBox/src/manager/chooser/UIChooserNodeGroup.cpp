20da8f261a41d08a6d0c95f61c2b027c236524cf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-09-29 08:49:24 +0000

    FE/Qt: bugref:10982: VBox Manager: Chooser pane: Syncing group Chooser item accessibility description with similar for machine Chooser item; It's useful for screen-readers as group name doesn't always mention group word.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110666 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserNodeGroup.cpp
+++ src/VBox/Frontends/VirtualBox/src/manager/chooser/UIChooserNodeGroup.cpp
@@ -116,7 +116,7 @@ QString UIChooserNodeGroup::fullName() const
 
 QString UIChooserNodeGroup::description() const
 {
-    return name();
+    return m_strDescription;
 }
 
 QString UIChooserNodeGroup::definition(bool fFull /* = false */) const
@@ -301,6 +301,9 @@ QUuid UIChooserNodeGroup::id() const
 
 void UIChooserNodeGroup::sltRetranslateUI()
 {
+    /* Update internal stuff: */
+    m_strDescription = tr("Machine Group");
+
     /* Update group-item: */
     if (item())
         item()->updateItem();
