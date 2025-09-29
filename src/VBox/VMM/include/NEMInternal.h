32209bcca9db7a123a62a6554f608e3709996061
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-09-29 07:10:18 +0000

    VMM/NEMR3Native-darwin-armv8.cpp: Try avoid searching all of aMmio2DirtyTracking in nemR3DarwinHandleExitExceptionDataAbort since we're unlikely to have more than one entry in the table (VGA frame buffer). [missing file] jiraref:VBP-1886
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110664 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/include/NEMInternal.h
+++ src/VBox/VMM/include/NEMInternal.h
@@ -356,8 +356,12 @@ typedef struct NEM
     /** @} */
     /** Dirty tracking slots. */
     NEMHVMMIO2REGION            aMmio2DirtyTracking[8];
+    /** The end of valid aMmio2DirtyTracking entries. */
+    uint32_t                    idxMmio2DirtyTrackingEnd;
+    uint32_t                    u32Padding2;
     /** The vCPU config. */
     hv_vcpu_config_t            hVCpuCfg;
+
 # elif defined(VBOX_VMM_TARGET_X86)
     /** Set if hv_vm_space_create() was called successfully. */
     bool                        fCreatedAsid : 1;
