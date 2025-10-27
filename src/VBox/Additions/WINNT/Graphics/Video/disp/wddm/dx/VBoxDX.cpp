9daf98e317cb3f7bad34f6f0661f71e523999066
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-10-27 16:11:12 +0000

    WDDM: updated assertion
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110978 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/dx/VBoxDX.cpp
+++ src/VBox/Additions/WINNT/Graphics/Video/disp/wddm/dx/VBoxDX.cpp
@@ -2595,7 +2595,9 @@ void vboxDXDestroyQuery(PVBOXDX_DEVICE pDevice, PVBOXDXQUERY pQuery)
 
 void vboxDXQueryBegin(PVBOXDX_DEVICE pDevice, PVBOXDXQUERY pQuery)
 {
-    Assert(pQuery->enmQueryState == VBOXDXQUERYSTATE_CREATED || pQuery->enmQueryState == VBOXDXQUERYSTATE_SIGNALED);
+    Assert(   pQuery->enmQueryState == VBOXDXQUERYSTATE_CREATED
+           || pQuery->enmQueryState == VBOXDXQUERYSTATE_SIGNALED
+           || ((pQuery->svga.flags & SVGA3D_DXQUERY_FLAG_PREDICATEHINT) && pQuery->enmQueryState == VBOXDXQUERYSTATE_ISSUED));
 
     pQuery->enmQueryState = VBOXDXQUERYSTATE_BUILDING;
     if (pQuery->Query == D3D10DDI_QUERY_EVENT)
