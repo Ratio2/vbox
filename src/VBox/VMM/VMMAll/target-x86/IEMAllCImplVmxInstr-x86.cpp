86000c4b9e537b8d39e012647a8a4cece95b21e3
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-10-28 15:05:30 +0000

    VMM/IEM: github:gh-313 Fixed VMX nested-guest VM-entry which checked the wrong VM-execution control bit in the virtual VMCS.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@110991 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/target-x86/IEMAllCImplVmxInstr-x86.cpp
+++ src/VBox/VMM/VMMAll/target-x86/IEMAllCImplVmxInstr-x86.cpp
@@ -6674,7 +6674,7 @@ static int iemVmxVmentryCheckCtls(PVMCPUCC pVCpu, const char *pszInstr) RT_NOEXC
 
         /* Save preemption timer without activating it. */
         if (    (pVmcs->u32PinCtls & VMX_PIN_CTLS_PREEMPT_TIMER)
-            || !(pVmcs->u32ProcCtls & VMX_EXIT_CTLS_SAVE_PREEMPT_TIMER))
+            || !(pVmcs->u32ExitCtls & VMX_EXIT_CTLS_SAVE_PREEMPT_TIMER))
         { /* likely */ }
         else
             IEM_VMX_VMENTRY_FAILED_RET(pVCpu, pszInstr, pszFailure, kVmxVDiag_Vmentry_SavePreemptTimer);
