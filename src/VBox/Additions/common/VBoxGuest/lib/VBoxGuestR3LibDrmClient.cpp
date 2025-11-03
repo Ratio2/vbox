a6b135b262a0f6766af0fa47cad5d82dd0ffbd05
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-11-03 22:56:34 +0000

    additions/common/VBoxGuestR3LibDrmClient.cpp: nits. bugref:10134 github:gh-311
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@111011 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibDrmClient.cpp
+++ src/VBox/Additions/common/VBoxGuest/lib/VBoxGuestR3LibDrmClient.cpp
@@ -66,14 +66,13 @@
 static bool vbglR3DrmClientCheckProp(const char *pszPropName, uint32_t fPropFlags)
 {
     bool fExist = false;
-# if defined(VBOX_WITH_GUEST_PROPS)
-    uint32_t idClient;
 
+# if defined(VBOX_WITH_GUEST_PROPS)
+    uint32_t idClient = 0;
     int rc = VbglR3GuestPropConnect(&idClient);
     if (RT_SUCCESS(rc))
     {
         char *pcszFlags = NULL;
-
         rc = VbglR3GuestPropReadEx(idClient, pszPropName, NULL /* ppszValue */, &pcszFlags, NULL);
         if (RT_SUCCESS(rc))
         {
@@ -81,9 +80,8 @@ static bool vbglR3DrmClientCheckProp(const char *pszPropName, uint32_t fPropFlag
             if (fPropFlags)
             {
                 uint32_t fFlags = 0;
-
                 rc = GuestPropValidateFlags(pcszFlags, &fFlags);
-                fExist = RT_SUCCESS(rc) && (fFlags == fPropFlags);
+                fExist = RT_SUCCESS(rc) && fFlags == fPropFlags;
             }
             else
                 fExist = true;
