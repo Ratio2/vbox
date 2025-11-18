bbe359f950f4a74bb4193a5b057f08a7d8260b2c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-11-18 05:19:26 +0000

    VMM: Move arm-specific GIC ITS code to target-armv8 directory.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@111230 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMAll/target-armv8/GICAll-armv8.cpp
+++ src/VBox/VMM/VMMAll/target-armv8/GICAll-armv8.cpp
@@ -46,7 +46,8 @@
  * presence is optional. It provides MSI support along with routing interrupt
  * sources to specific PEs. The ITS is only accessible via its memory mapped I/O
  * region. When the MMIO handle for the its region is NIL_IOMMMIOHANDLE it's
- * considered to be disabled for the VM. Most of the ITS code lives in GITSAll.cpp.
+ * considered to be disabled for the VM. Most of the ITS code lives in
+ * GITSAll-armv8.cpp.
  *
  * This implementation only targets GICv3. This implementation does not support
  * dual security states, nor does it support exception levels (EL2, EL3). Earlier
