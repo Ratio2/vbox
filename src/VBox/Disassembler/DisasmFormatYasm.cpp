1bc2dd797845a0c94f994ee3c3c487164b4ff3bf
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-12-01 11:43:11 +0000

    Disassembler: Get rid of GC_ARCH_BITS and assume 64 everywhere, which it is for a long time now, bugref:11013
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@111361 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Disassembler/DisasmFormatYasm.cpp
+++ src/VBox/Disassembler/DisasmFormatYasm.cpp
@@ -425,10 +425,8 @@ DISDECL(size_t) DISFormatYasmEx(PCDISSTATE pDis, char *pszBuf, size_t cchBuf, ui
      */
     if (fFlags & DIS_FMT_FLAGS_ADDR_LEFT)
     {
-#if HC_ARCH_BITS == 64 || GC_ARCH_BITS == 64
         if (pDis->uInstrAddr >= _4G)
             PUT_NUM(9, "%08x`", (uint32_t)(pDis->uInstrAddr >> 32));
-#endif
         PUT_NUM(8, "%08x", (uint32_t)pDis->uInstrAddr);
         PUT_C(' ');
     }
@@ -1318,10 +1316,8 @@ DISDECL(size_t) DISFormatYasmEx(PCDISSTATE pDis, char *pszBuf, size_t cchBuf, ui
         if (fFlags & DIS_FMT_FLAGS_ADDR_RIGHT)
         {
             PUT_C(' ');
-#if HC_ARCH_BITS == 64 || GC_ARCH_BITS == 64
             if (pDis->uInstrAddr >= _4G)
                 PUT_NUM(9, "%08x`", (uint32_t)(pDis->uInstrAddr >> 32));
-#endif
             PUT_NUM(8, "%08x", (uint32_t)pDis->uInstrAddr);
         }
 
