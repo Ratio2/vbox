b1f41cb9d34d3bd3e440b21d59b3289d02df119e
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-12-05 16:01:58 +0000

    FE/Qt: bugref:10681: QITableWidget: Fixing macOS regression of r171719; macOS doesn't support tables or trees and their items.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@111449 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/Frontends/VirtualBox/src/extensions/QITableWidget.cpp
+++ src/VBox/Frontends/VirtualBox/src/extensions/QITableWidget.cpp
@@ -62,8 +62,12 @@ public:
     /** Returns the role. */
     virtual QAccessible::Role role() const RT_OVERRIDE
     {
-        /* Cell in any case: */
-        return QAccessible::Cell;
+#ifdef VBOX_WS_MAC
+            // WORKAROUND: macOS doesn't respect QAccessible::Table/Cell roles.
+            return QAccessible::ListItem;
+#else
+            return QAccessible::Cell;
+#endif
     }
 
     /** Returns the parent. */
@@ -206,7 +210,12 @@ public:
 
     /** Constructs an accessibility interface passing @a pWidget to the base-class. */
     QIAccessibilityInterfaceForQITableWidget(QWidget *pWidget)
+#ifdef VBOX_WS_MAC
+        // WORKAROUND: macOS doesn't respect QAccessible::Table/Cell roles.
+        : QAccessibleWidget(pWidget, QAccessible::List)
+#else
         : QAccessibleWidget(pWidget, QAccessible::Table)
+#endif
     {}
 
     /** Returns a specialized accessibility interface @a enmType. */
