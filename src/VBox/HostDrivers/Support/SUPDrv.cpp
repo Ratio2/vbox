4d585fe78c2fa8fb881a1e407c4ea17b2dfb445c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-12-05 01:30:45 +0000

    SUPDrv: Disabled reading TRCDEVARCH as that causes trouble on A725 or/and X925 CPUs (probably misdetected feature).
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@111431 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/HostDrivers/Support/SUPDrv.cpp
+++ src/VBox/HostDrivers/Support/SUPDrv.cpp
@@ -7436,9 +7436,11 @@ static void supdrvIOCtl_ArmGetSysRegsOnCpu(PSUPARMGETSYSREGS pReq, uint32_t cons
     /** @todo FEAT_ETE: READ_SYS_REG_NAMED(2, 1, 0,  4, 6, TRCIDR12);  */
     /** @todo FEAT_ETE: READ_SYS_REG_NAMED(2, 1, 0,  5, 6, TRCIDR13);  */
 
+# if 0 /** @todo this crashes on DGX  */
     bool const fFeatEte = ((fDfr0 >> 4) & 0xfU) >= 1U; /* FEAT_ETE <-> ID_AA64DFR0_EL1.TraceVer >= 1 */
     if (fFeatEte)
         READ_SYS_REG_NAMED(2, 1, 7, 15, 6, TRCDEVARCH);
+# endif        
 
     /*
      * Collections of other read-only registers.
