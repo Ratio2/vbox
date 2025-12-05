4e7ac4af44303c363b6da538bac2a5ce58ad7c0c
vboxsync <vboxsync@cfe28804-0f27-0410-a406-dd0f0b0b656f>
2025-12-05 01:38:20 +0000

    VMM/CPUM: Added profile for an ARM Cortex-X925/A725 cpu.
    git-svn-id: https://www.virtualbox.org/svn/vbox/trunk@111433 cfe28804-0f27-0410-a406-dd0f0b0b656f

--- src/VBox/VMM/VMMR3/CPUMR3Db.cpp
+++ src/VBox/VMM/VMMR3/CPUMR3Db.cpp
@@ -133,6 +133,7 @@ static int cpumDbPopulateInfoFromEntry(PCPUMINFO pInfo, PCCPUMDBENTRY pEntryCore
 # include "cpus/ARM_Apple_M2_Max.h"
 # include "cpus/ARM_Apple_M3_Max.h"
 # include "cpus/ARM_Qualcomm_Snapdragon_X.h"
+# include "cpus/ARM_ARM_Cortex_X925_A725.h"
 
 #endif
 
@@ -255,6 +256,7 @@ static CPUMDBENTRY const * const g_apCpumDbEntries[] =
     &g_Entry_ARM_Apple_M2_Max.Core,
     &g_Entry_ARM_Apple_M3_Max.Core,
     &g_Entry_ARM_Qualcomm_Snapdragon_X.Core,
+    &g_Entry_ARM_ARM_Cortex_X925_A725.Core,
 #endif /* VBOX_VMM_TARGET_ARMV8 || RT_ARCH_ARM64 */
 };
 
